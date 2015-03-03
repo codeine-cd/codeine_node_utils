643de39746a0955a30c518cac2253f8a30ab2ac9
oshai <ohadshai@gmail.com>
2015-03-03 11:03:19 +0200

    Update codeine.test.js

--- test/codeine.test.js
+++ test/codeine.test.js
@@ -1,6 +1,6 @@
 var codeine = require('./../lib');
 
-var client = new codeine('codeine.intel.com',12347,'82015144-d17b-4bee-96e3-d0f9b97440bb');
+var client = new codeine('codeine.intel.com',12347,'');
 
 client.getProjectNodes('ion-proxy').then(function(data) {
    console.dir(data);
