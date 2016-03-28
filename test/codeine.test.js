fdce7be5e67cc170a7c5cca65530fdf6e6d2493b
Roi Ezra <ezraroi@gmail.com>
2016-03-28 14:42:43 +0300

    fixed runCommand

--- test/codeine.test.js
+++ test/codeine.test.js
@@ -1,7 +1,16 @@
 var codeine = require('./../lib');
 
-var client = new codeine('codeine.intel.com',12347,'');
+var client = new codeine('codeine.intel.com',12347,'772c9736-b3d7-485e-97f3-adffc99d7569');
 
-client.getProjectNodes('ion-proxy').then(function(data) {
-   console.dir(data);
-});
+// client.getProjectNodes('Compute_Process_Monitor').then(function(data) {
+//    console.dir(data);
+// });
+
+var commandObject = {
+   command_info : {
+      project_name : 'Compute_Process_Monitor',
+      name : 'list processes'
+   },
+   should_execute_on_all_nodes : true
+};
+client.runCommand('Compute_Process_Monitor', commandObject);
